16ff54d8665e7da929df348319163b073e3a1d57
Shrravan Balamurugan <shrravan.bala@gmail.com>
2026-06-26 15:47:06 +1000

    Small adjustments to skills and experiences

--- src/components/sections/SkillsSection.tsx
+++ src/components/sections/SkillsSection.tsx
@@ -7,12 +7,12 @@ const skills = [
   { name: "Tailwind CSS", score: 9 },
   { name: "Node.js", score: 8 },
   { name: "UI/UX Design", score: 9 },
-  { name: "Python", score: 7 },
+  { name: "Python", score: 8 },
   { name: "Docker", score: 6 },
   { name: "Kubernetes", score: 8},
   { name: "Terraform", score: 7},
   { name: "AWS", score: 8},
-  { name: "Serverless", score: 7}
+  { name: "Serverless", score: 7},
 ];
 
 const SkillsSection = () => {
